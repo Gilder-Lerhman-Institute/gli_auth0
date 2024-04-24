3446f4368913fa0efc824bbfc1af9e1f4d52e4ca
Sean Dietrich <sean@kanopi.com>
2024-04-24 12:28:37 -0700

    Updated profile registration to move custom functions outside of attachment block

--- modules/gli_auth0_profile/js/registration.js
+++ modules/gli_auth0_profile/js/registration.js
@@ -1,5 +1,22 @@
 (function(Drupal, drupalSettings) {
   'use strict';
+
+  Drupal.gliAuth0Profile = Drupal.gliAuth0Profile || {};
+  Drupal.gliAuth0Profile.setRedirect = function(redirectUrl) {
+    // Go to the endpoint and if the response is ok then redirect to the provided destination.
+    setTimeout(function() {
+      fetch("/auth0/registration-complete")
+        .then((response) => response.text())
+        .then(function(text) {
+          if (text === "ok") {
+            window.location = redirectUrl;
+          } else {
+            Drupal.gliAuth0Profile.setRedirect(redirectUrl);
+          }
+        });
+    }, 2000);
+  };
+  
   Drupal.behaviors.completeListener = {
     attach: function () {
       const init = function(){
@@ -16,22 +33,6 @@
         );
       };
       setTimeout(init, 100);
-
-      Drupal.gliAuth0Profile = Drupal.gliAuth0Profile || {};
-      Drupal.gliAuth0Profile.setRedirect = function(redirectUrl) {
-        // Go to the endpoint and if the response is ok then redirect to the provided destination.
-        setTimeout(function() {
-          fetch("/auth0/registration-complete")
-            .then((response) => response.text())
-            .then(function(text) {
-              if (text === "ok") {
-                window.location = redirectUrl;
-              } else {
-                Drupal.gliAuth0Profile.setRedirect(redirectUrl);
-              }
-            })
-        }, 2000);
-      };
 
       document.addEventListener("registration_complete", event => {
         Drupal.gliAuth0Profile.setRedirect(drupalSettings.gli_auth0_profile_registration.redirect_url ?? '/');
