3361d32bef36969914a8c2f186d1daaf68053914
Sean Dietrich <sean@kanopi.com>
2024-04-24 16:16:50 -0700

    Profile Templates (#3)
    * Initial development of profile_registration template
    
    * Added module alter for form data
    
    * Fixed name of library file
    
    * Fixed issues with preprocess form
    
    * Fixed reference to flow registration names
    
    * Fixed variable reference for the registration.js
    
    * Updated profile registration to move custom functions outside of attachment block
    
    * Updated app and comp names
    
    * Updated adjustments for drupalSettings

---
+++ modules/gli_auth0_profile/js/registration.js
@@ -0,0 +1,43 @@
+(function(Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.gliAuth0Profile = Drupal.gliAuth0Profile || {};
+  Drupal.gliAuth0Profile.setRedirect = function(redirectUrl) {
+    // Go to the endpoint and if the response is ok then redirect to the provided destination.
+    setTimeout(function() {
+      fetch("/auth0/registration-complete")
+        .then((response) => response.text())
+        .then(function(text) {
+          if (text === "ok") {
+            window.location = redirectUrl;
+          } else {
+            Drupal.gliAuth0Profile.setRedirect(redirectUrl);
+          }
+        });
+    }, 2000);
+  };
+
+  Drupal.behaviors.completeListener = {
+    attach: function () {
+      const init = function(){
+        $Lightning.use(drupalSettings.gli_auth0_profile_registration.app_name,
+          function() {
+            $Lightning.createComponent(
+              drupalSettings.gli_auth0_profile_registration.component_name,
+              drupalSettings.gli_auth0_profile_registration.form_data,
+              "container",
+              function(cmp) {}
+            );
+          },
+          drupalSettings.gli_auth0_profile_registration.experience_cloud
+        );
+      };
+      setTimeout(init, 100);
+
+      document.addEventListener("registration_complete", event => {
+        Drupal.gliAuth0Profile.setRedirect(drupalSettings.gli_auth0_profile_registration.redirect_url ?? '/');
+      });
+    }
+  };
+})(Drupal, drupalSettings);
+
