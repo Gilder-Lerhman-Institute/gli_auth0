417fd2fcfa966703ed1d56eb09b04561d43f1b6d
Wes Jones <waj194@gmail.com>
2026-02-26 16:59:27 -0500

    tw29998778 New registration flow - fall back to legacy flow if gli_registration is not enabled.

--- modules/gli_auth0_profile/src/EventSubscriber/ProfileCompletedEventSubscriber.php
+++ modules/gli_auth0_profile/src/EventSubscriber/ProfileCompletedEventSubscriber.php
@@ -2,12 +2,15 @@
 
 namespace Drupal\gli_auth0_profile\EventSubscriber;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Routing\RouteObjectInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\Url;
 use Drupal\gli_auth0\Service\Auth0Service;
+use Drupal\user\UserDataInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -58,6 +61,27 @@ class ProfileCompletedEventSubscriber implements EventSubscriberInterface {
   protected $masquerade;
 
   /**
+   * The module handler service.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected ModuleHandlerInterface $moduleHandler;
+
+  /**
+   * The user data service.
+   *
+   * @var \Drupal\user\UserDataInterface
+   */
+  protected UserDataInterface $userData;
+
+  /**
+   * The config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected ConfigFactoryInterface $configFactory;
+
+  /**
    * Constructor.
    *
    * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
@@ -70,19 +94,31 @@ class ProfileCompletedEventSubscriber implements EventSubscriberInterface {
    *   Messenger Service.
    * @param null $masquerade
    *   Masquerade Service.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler service.
+   * @param \Drupal\user\UserDataInterface $user_data
+   *   The user data service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
    */
   public function __construct(
     AccountProxyInterface $currentUser,
     Auth0Service $auth0Service,
     RequestStack $requestStack,
     MessengerInterface $messenger,
-    $masquerade = NULL
+    $masquerade = NULL,
+    ModuleHandlerInterface $module_handler = NULL,
+    UserDataInterface $user_data = NULL,
+    ConfigFactoryInterface $config_factory = NULL
   ) {
     $this->currentUser = $currentUser;
     $this->auth0Service = $auth0Service;
     $this->request = $requestStack->getCurrentRequest();
     $this->messenger = $messenger;
     $this->masquerade = $masquerade;
+    $this->moduleHandler = $module_handler;
+    $this->userData = $user_data;
+    $this->configFactory = $config_factory;
   }
 
   /**
@@ -107,7 +143,9 @@ class ProfileCompletedEventSubscriber implements EventSubscriberInterface {
       // 'gli_auth0_profile.profile_update',
       'gli_auth0_profile.complete_registration',
       'gli_auth0.logout',
-      'gli_auth0_profile.registration_done'
+      'gli_auth0_profile.registration_done',
+      // Gli Registration Routes:
+      'gli_registration.form',
     ]);
 
     // Ignore route for jsonapi calls.
@@ -129,23 +167,41 @@ class ProfileCompletedEventSubscriber implements EventSubscriberInterface {
         }
       }
 
-      $registration_complete = FALSE;
       $auth0Id = $this->auth0Service->getUserAuth0Id($this->currentUser->id());
       $is_auth0_user = $auth0Id !== NULL;
 
-      if ($is_auth0_user) {
-        $registration_complete = $this->auth0Service->isRegistrationComplete($auth0Id);
+      if (!$is_auth0_user) {
+        return;
       }
 
-      if ($is_auth0_user && !$registration_complete) {
-        $url = gli_auth0_profile_get_registration_url(
-          Url::fromUserInput($event->getRequest()->getRequestUri())->toString()
-        );
-        $url = $url->setAbsolute()->toString();
-        $event->setResponse(new RedirectResponse($url));
-        $this->messenger->addError(
-          $this->t('Registration must be completed before unlocking other parts of the site.')
-        );
+      $registration_complete = $this->auth0Service->isRegistrationComplete($auth0Id);
+      if (!$registration_complete) {
+        if ($this->moduleHandler && $this->moduleHandler->moduleExists('gli_registration')) {
+          // New flow: check local userData flag (provisional completion).
+          $config = $this->configFactory->get('gli_registration.settings');
+          if ($config->get('allow_registered_users')) {
+            return;
+          }
+          $completed = $this->userData->get('gli_registration', $this->currentUser->id(), 'completed');
+          if (!$completed) {
+            $redirect_url = Url::fromRoute('gli_registration.form')->setAbsolute()->toString();
+            $event->setResponse(new RedirectResponse($redirect_url));
+            $this->messenger->addError(
+              $this->t('Registration must be completed before unlocking other parts of the site.')
+            );
+          }
+        }
+        else {
+          // Legacy flow.
+          $url = gli_auth0_profile_get_registration_url(
+            Url::fromUserInput($event->getRequest()->getRequestUri())->toString()
+          );
+          $url = $url->setAbsolute()->toString();
+          $event->setResponse(new RedirectResponse($url));
+          $this->messenger->addError(
+            $this->t('Registration must be completed before unlocking other parts of the site.')
+          );
+        }
       }
     }
   }
---
+++ modules/gli_auth0_profile/src/EventSubscriber/RegistrationAccessSubscriber.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Drupal\gli_auth0_profile\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Messenger\MessengerInterface;
+use Drupal\Core\Routing\RouteObjectInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+use Drupal\gli_auth0\Service\Auth0Service;
+use Drupal\user\UserDataInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Blocks completed users from re-accessing the registration form.
+ */
+class RegistrationAccessSubscriber implements EventSubscriberInterface {
+
+  use StringTranslationTrait;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user.
+   * @param \Drupal\gli_auth0\Service\Auth0Service $auth0Service
+   *   The Auth0 service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The request stack.
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   * @param \Drupal\user\UserDataInterface $userData
+   *   The user data service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
+   *   The module handler.
+   */
+  public function __construct(
+    protected AccountProxyInterface $currentUser,
+    protected Auth0Service $auth0Service,
+    protected RequestStack $requestStack,
+    protected MessengerInterface $messenger,
+    protected UserDataInterface $userData,
+    protected ConfigFactoryInterface $configFactory,
+    protected ModuleHandlerInterface $moduleHandler,
+  ) {
+  }
+
+  /**
+   * Redirect completed users away from the registration form.
+   */
+  public function onRequest(RequestEvent $event) {
+    $request = $this->requestStack->getCurrentRequest();
+    $route_name = $request->attributes->get(RouteObjectInterface::ROUTE_NAME);
+
+    if ($route_name !== 'gli_registration.form') {
+      return;
+    }
+
+    if (!$this->moduleHandler->moduleExists('gli_registration')) {
+      return;
+    }
+
+    if ($this->currentUser->isAnonymous()) {
+      return;
+    }
+
+    $config = $this->configFactory->get('gli_registration.settings');
+    if ($config->get('allow_registered_users')) {
+      return;
+    }
+
+    $auth0Id = $this->auth0Service->getUserAuth0Id($this->currentUser->id());
+    if (!$auth0Id) {
+      return;
+    }
+
+    $completed = $this->auth0Service->isRegistrationComplete($auth0Id)
+      || $this->userData->get('gli_registration', $this->currentUser->id(), 'completed');
+
+    if ($completed) {
+      $event->setResponse(new RedirectResponse(
+        Url::fromRoute('<front>')->setAbsolute()->toString()
+      ));
+      $this->messenger->addStatus(
+        $this->t('Registration has already been completed.')
+      );
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::REQUEST][] = ['onRequest', 20];
+    return $events;
+  }
+
+}
